2cd0e017dc6188c333065fa90ab50833acc31cf4
Brian Slate <brians@cardintegrators.com>
2023-09-05 01:18:53 -0700

    stuff

---
+++ src/lib/AdditionalInfo.svelte
@@ -0,0 +1,78 @@
+<script>
+	export let resumeData;
+
+	function handleMouseOver(event) {
+		const icon = event.currentTarget.querySelector('.info-icon');
+		icon.style.color = '#FFD700'; // Your theme gold color
+	}
+</script>
+
+<section id="additional-info">
+	<h2 class="section-title">Additional Information</h2>
+	<ul class="info-list">
+		{#each resumeData.additionalInfo as info}
+			<li class="info-item" on:mouseover={handleMouseOver}>
+				<span class="info-icon">✔</span>
+				<span class="info-text">{info}</span>
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	#additional-info {
+		background-color: #f9f9f9;
+		padding: 20px;
+		border-radius: 10px;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	}
+
+	.section-title {
+		font-size: 1.5em;
+		font-weight: bold;
+		color: #333;
+		text-transform: uppercase;
+		margin-bottom: 20px;
+	}
+
+	.info-list {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	.info-item {
+		display: flex;
+		align-items: center;
+		margin-bottom: 10px;
+		cursor: pointer;
+	}
+
+	.info-icon {
+		font-size: 1.2em;
+		color: #4CAF50;
+		margin-right: 10px;
+		transition: all 0.3s ease;
+	}
+
+	.info-item:hover .info-icon {
+		animation: pulse 0.3s ease-in-out;
+	}
+
+	@keyframes pulse {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(3);
+		}
+		100% {
+			transform: scale(1);
+		}
+	}
+
+	.info-text {
+		font-size: 1em;
+		color: #333;
+	}
+</style>
---
+++ src/lib/ProfileHeader.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let resumeData;
+  </script>
+  
+  <header class="header">
+    <img src="https://media.licdn.com/dms/image/D5603AQEpuj47L8XjNQ/profile-displayphoto-shrink_200_200/0/1686328359598?e=1699488000&v=beta&t=cQL7G2lWkmbm_nmMZGiizJQX1qlKWzjqxm5SgI1pNNc" alt="Brian T. Slate Profile Photo">
+    <div class="header-text">
+      <h1>{resumeData.name}</h1>
+      <p>{resumeData.title}</p>
+    </div>
+  </header>
+  
+  <style>
+    header {
+        column-span: all;
+        background-color: #f3f3f3;
+        padding: 1.5rem 2rem;
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 8px solid var(--color-theme-5);
+        border-radius: 10px;
+    }
+
+    .header {
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+      background-color: rgba(255, 255, 255, 0.6);
+      margin-bottom: 1.5rem;
+    }
+  
+    .header::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-image: url('/background.webp');
+      background-size: cover;
+      background-repeat: repeat;
+      opacity: 0.2;
+      z-index: -1;
+    }
+  
+    .header-text h1 {
+      font-size: 2.5em; /* Makes the name larger */
+    }
+  
+    .header-text p {
+      font-size: 1.5em; /* Makes the title larger */
+    }
+  </style>
+  
