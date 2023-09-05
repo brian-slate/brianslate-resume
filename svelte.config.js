a679a113e309e91f747bd27de44650fbf154493f
Brian Slate <brians@cardintegrators.com>
2023-09-05 01:24:12 -0700

    prerender for page.js

--- svelte.config.js
+++ svelte.config.js
@@ -16,15 +16,12 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '<div>Loading...</div>'
-		}),
-		files: {
-			routes: 'src/routes'
-		}
-	}
+    kit: {
+        adapter: adapter(),
+        files: {
+            routes: 'src/routes'
+        }
+    }
 };
 
 export default config;
-
